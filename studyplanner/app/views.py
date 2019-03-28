4d50f03848c05c3b3884fa9e091f9874f47aa05c
Alice Alves <a.araujo-alves@uea.ac.uk>
2019-03-28 02:27:11 +0000

    deadlines view html completed

--- studyplanner/app/views.py
+++ studyplanner/app/views.py
@@ -36,8 +36,32 @@ def uploadHubFile(request):
 
     return redirect('/dashboard')
 def deadlines(request):
-	context = {
-		'navigation': navigation_list,
-		'active': 'Deadlines'
-	}
-	return render(request, 'deadlines.html', context)
+    upcoming = [
+        {'name':'Software Engineering coursework 2', 'date':'15/04/2019','progress':30},
+        {'name':'Graphics Coursework', 'date':'26/04/2019','progress':0},
+        {'name':'Programming coursework 2', 'date':'01/05/2019','progress':90}
+    ]
+    inprogress = [
+        {'name':'Software Engineering coursework 2', 'date':'15/04/2019','progress':30},
+        {'name':'Programming coursework 2', 'date':'01/05/2019','progress':90}
+    ]
+    missed = [
+        {'name':'Database Structures and Algorithms coursework', 'date':'20/03/2019','progress':80}
+    ]
+    completed = [
+        {'name':'Software Engineering coursework 1', 'date':'15/03/2019','progress':100},
+        {'name':'Programming coursework 1', 'date':'26/02/2019','progress':100},
+        {'name':'Programming coursework 1', 'date':'26/02/2019','progress':100},
+        {'name':'Programming coursework 1', 'date':'26/02/2019','progress':100},
+        {'name':'Programming coursework 1', 'date':'26/02/2019','progress':100}
+    ]
+    context = {
+        'navigation': navigation_list,
+        'active': 'Deadlines',
+        'upcoming' : upcoming,
+        'inprogress' : inprogress,
+        'missed' : missed,
+        'completed' : completed
+    }
+    deadlines = { 'upcoming':upcoming,'inprogress':inprogress,'missed':missed,'completed':completed}
+    return render(request, 'deadlines.html', context)
