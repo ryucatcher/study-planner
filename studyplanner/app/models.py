df368357e7bddac465e8c11088a88b855796c154
Alice Alves <a.araujo-alves@uea.ac.uk>
2019-04-01 23:32:24 +0100

    task view now shows data from database

--- studyplanner/app/models.py
+++ studyplanner/app/models.py
@@ -62,7 +62,6 @@ class Assessment(models.Model):
         progress = 0.0
         for t in tasks:
             progress += t.progress()/size
-            print(progress)
         return progress
     def __str__(self):
         return self.name
@@ -80,7 +79,6 @@ class StudyTask(models.Model):
         progress = 0.0
         for a in activities:
             progress += a.progress()/size
-            print(progress)
         return progress
     def __str__(self):
         return self.name
